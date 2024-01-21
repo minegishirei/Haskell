1ea52a9755c3dbf13ebdae6573f720941376c0bf
minegishirei <minegishirei@gmail.com>
2024-01-21 13:28:31 +0900

    save

---
+++ code/sql/main.hs
@@ -0,0 +1,22 @@
+
+
+
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Applicative
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+
+data TestField = TestField Int String deriving (Show)
+
+instance FromRow TestField where
+  fromRow = TestField <$> field <*> field
+
+main :: IO ()
+main = do
+  conn <- open "test.db"
+  execute conn "CREATE TABLE IF NOT EXISTS TEST (id INTEGER PRIMARY KEY, str text);" ()
+  execute conn "INSERT INTO TEST (str) VALUES (?)" (Only ("test string 2" :: String))
+  r <- query_ conn "SELECT * from TEST" :: IO [TestField]
+  mapM_ print r
+  close conn
+
---
+++ code/types/main.hs
@@ -0,0 +1,7 @@
+
+divBy2 :: Int -> Float
+divBy2 x = x / 2
+
+
+main = do
+    print( divBy2 1)
